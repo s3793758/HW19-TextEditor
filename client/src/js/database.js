3be56fe3d4e6a30cb52976227e25b1efb3c567da
managment_system <mas152q@gmail.com>
2022-07-14 21:42:13 +1000

    adding datat

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,26 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) =>
+  console.error('Error Insert not implemented');
+
+const jateDb = await openDB('jate', 1);
+const tx = jateDb.transaction('jate', readwrite);
+const store = tx.objectStore('jate');
+
+let request = store.put({ id: 1, value: content });
+let result = await request;
+if (!result) {
+  request = store.add({ value: content });
+  result = await request;
+}
+
+console.log('Results saved', result);
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => console.error('Error retriving Data failed');
+
+// const jateDb = await openDB
+
 
 initdb();
